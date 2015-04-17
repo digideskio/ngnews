4ecd105ff7d96c1709db4ad854bfa906ba7c4cff
Robert Kuśmirek <kusmirek.robert@gmail.com>
2015-04-17 18:29:10 +0200

    remove empty index action

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,7 +2,4 @@ class SettingsController < ApplicationController
   expose(:categories_repository) { CategoriesRepository.new }
   expose(:categories) { categories_repository.all }
   expose(:item)
-
-  def index
-  end
 end
