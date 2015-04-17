099ea5624706e2897addb810bfa25ca1a3666246
Adam Nowak <adam.nowak@netguru.co>
2015-04-17 17:44:09 +0200

    icons for categories

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -8,4 +8,24 @@ class ItemDecorator < Draper::Decorator
     return if from_time.nil?
     "#{distance_of_time_in_words(from_time, Time.zone.today)} ago"
   end
+
+  def icon_name
+    icons[item.category || item.subcategory] || random_icon
+  end
+
+  def random_icon
+    %w(ss-award ss-ribbon ss-medal ss-crayons ss-pencilbrush ss-lightning).sample
+  end
+
+  def icons
+    {
+      people: "ss-usergroup",
+      link: "ss-link",
+      props: "ss-like",
+      blog: "ss-notice",
+      event: "ss-calendar",
+      office: "ss-warehouse",
+      misc: "ss-whale",
+    }
+  end
 end
