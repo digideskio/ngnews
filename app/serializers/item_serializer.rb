9d818a4f36cdcebd0b32b3cb02a754d4a06f2de3
Adam Nowak <adam.nowak@netguru.co>
2015-04-17 16:40:27 +0200

    add start date by default and remove category validation for now

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,3 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :category, :source, :body
+  attributes :id, :category, :sub_category, :start_date, :source, :body
 end
--- app/use_cases/items/create.rb
+++ app/use_cases/items/create.rb
@@ -10,40 +10,12 @@ module Items
     end
 
     def call
+      attrs[:start_date] ||= Date.today
       item = items_repository.add attrs
-      item.validate
-      ItemCategoryValidator.new(item, categories_repository).validate!
-      if item.errors.empty?
+      if item.valid?
         Response::Success.new(data: item)
       else
         Response::Error.new(errors: item.errors.full_messages)
-      end
-    end
-
-    class ItemCategoryValidator
-      attr_accessor :item, :categories_repository
-
-      def initialize(item, categories_repository)
-        self.item = item
-        self.categories_repository = categories_repository
-      end
-
-      def validate!
-        validate_category! && validate_sub_category!
-      end
-
-      private
-
-      def validate_category!
-        return true if categories_repository.include? item.category
-        item.errors.add :category, "is not allowed"
-      end
-
-      def validate_sub_category!
-        return true if item.sub_category.blank? ||
-                       categories_repository.subcategories_for(item.category)
-                       .include?(item.sub_category)
-        item.errors.add :sub_category, "is not allowed"
       end
     end
   end
