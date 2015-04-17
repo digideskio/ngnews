5ecb593fdd152b1a65ab15dd0f1f121e238e1bc1
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-04-17 18:17:42 +0200

    expose categories and filter if q present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   expose(:items_repository) { ItemsRepository.new }
-  expose(:items) { ItemDecorator.decorate_collection(items_repository.all) }
+  expose(:items) { ItemDecorator.decorate_collection(items_repository.filter_by(query_params)) }
+  expose(:categories_repository) { CategoriesRepository.new }
+  expose(:categories) { categories_repository.all }
 
-  def index
+  private
+
+  def query_params
+    cats = CategoriesRepository.new.all.map(&:to_sym)
+    params.fetch(:q, {}).permit(*cats)
   end
 end
