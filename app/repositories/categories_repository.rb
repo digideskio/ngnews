89f0f69962eb17122d76a24d9f7bcb0b189d2f55
Adam Nowak <adam.nowak@netguru.co>
2015-04-17 15:23:24 +0200

    validate subcategories

--- app/repositories/categories_repository.rb
+++ app/repositories/categories_repository.rb
@@ -11,5 +11,18 @@ class CategoriesRepository
     )
   end
 
+  def subcategories_for(category_name)
+    @subcategories ||= build_subcategories
+    @subcategories[category_name.to_sym] || []
+  end
+
   delegate :include?, to: :all
+
+  private
+
+  def build_subcategories
+    {
+      people: %w(new promotion goodbye),
+    }
+  end
 end
--- app/use_cases/items/create.rb
+++ app/use_cases/items/create.rb
@@ -11,7 +11,8 @@ module Items
 
     def call
       item = items_repository.add attrs
-      if item.valid? && valid_item_category?(item)
+      ItemCategoryValidator.new(item, items_repository).validate!
+      if item.errors.empty?
         Response::Success.new(data: item)
       else
         Response::Error.new(errors: item.errors.full_messages)
@@ -20,10 +21,34 @@ module Items
 
     private
 
-    def valid_item_category?(item)
-      return true if categories_repository.include? item.category
-      item.errors.add :category, "is not allowed"
-      false
+    def valid_item_category?(_item)
+    end
+
+    class ItemCategoryValidator
+      attr_accessor :item, :categories_repository
+
+      def initialize(item, categories_repository)
+        self.item = item
+        self.categories_repository = categories_repository
+      end
+
+      def validate!
+        validate_category! && validate_sub_category!
+      end
+
+      private
+
+      def validate_category!
+        return if categories_repository.include? item.category
+        item.errors.add :category, "is not allowed"
+      end
+
+      def validate_sub_category!
+        return if item.sub_category.blank? ||
+                  categories_repository.subcategories_for(category)
+                  .include?(sub_category)
+        item.errors.add :sub_category, "is not allowed"
+      end
     end
   end
 end
