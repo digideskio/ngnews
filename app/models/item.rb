bc2a97ec81c071cddefec2ee0bf70a901db2c166
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-04-17 16:02:24 +0200

    add is_#cat#? methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ActiveRecord::Base
   validates :body, :category, :start_date, presence: true
+
+  # rubocop:disable Rails/FindEach
+  CategoriesRepository.new.all.each do |cat|
+    define_method "is_#{cat}?" do
+      category == cat
+    end
+  end
+  # rubocop:enable Rails/FindEach
 end
