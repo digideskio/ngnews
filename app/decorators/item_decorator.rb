4b8d5125b161e633f12c221070dbf87365a5e80b
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-04-17 16:01:14 +0200

    add ItemDecorator#display_date

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -1,3 +1,11 @@
 class ItemDecorator < Draper::Decorator
+  include ActionView::Helpers::DateHelper
+
   delegate :body, to: :item
+
+  def display_date
+    from_time = (object.is_event? ? object.end_date : object.start_date).to_time
+    return if from_time.nil?
+    "#{distance_of_time_in_words(from_time, Time.zone.today)} ago"
+  end
 end
--- spec/decorators/item_decorator_spec.rb
+++ spec/decorators/item_decorator_spec.rb
@@ -1,12 +1,18 @@
 require "rails_helper"
 
 describe ItemDecorator do
-  let(:item) { build :item }
+  let(:item) { build :item, start_date: 2.days.ago }
   subject { item.decorate }
 
   describe "#body" do
     it "delegates to item" do
       expect(subject.body).to eq item.body
+    end
+  end
+
+  describe "#display_date" do
+    it "displays time ago in words" do
+      expect(subject.display_date).to eq "2 days ago"
     end
   end
 end
