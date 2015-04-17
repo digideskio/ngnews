7b5f9cade8a941039e2214f9c56da30f62455f04
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-04-17 18:17:13 +0200

    add #filter_by to items repo

--- app/repositories/items_repository.rb
+++ app/repositories/items_repository.rb
@@ -8,4 +8,10 @@ class ItemsRepository
   def all
     Item.all.order(start_date: :desc)
   end
+
+  def filter_by(cat_names)
+    return all unless cat_names.any?
+    cats = cat_names.keys
+    Item.where(category: cats).order(start_date: :desc)
+  end
 end
