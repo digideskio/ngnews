03f9c12930e921edc2ee58d8771e8169069bb5bb
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-04-17 12:38:51 +0200

    fix typo

--- db/migrate/20150417103227_change_items_private_to_is_private.rb
+++ db/migrate/20150417103227_change_items_private_to_is_private.rb
@@ -1,5 +1,5 @@
 class ChangeItemsPrivateToIsPrivate < ActiveRecord::Migration
   def change
-    rename_column :items, :public, ;:is_public
+    rename_column :items, :public, :is_public
   end
 end
