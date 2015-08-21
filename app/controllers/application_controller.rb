b1ba4ebab2bf045025ef6d54ea74d4e6b90a02a8
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-08-21 13:14:21 +0200

    knock setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Knock::Authenticable
   protect_from_forgery with: :exception
+  before_action :authenticate
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
