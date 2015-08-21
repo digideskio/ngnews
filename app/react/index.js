b3a364f74156f3660e91035c22a13677d5440944
Sebastian Brych <sebastian.brych@netguru.pl>
2015-08-21 14:25:51 +0200

    App react registering components template

---
+++ app/react/index.js
@@ -0,0 +1,6 @@
+window.React = require('react');
+window.ReactComponents = {
+  //Users: require('./components/users'),
+  //'projects/index': require('./components/projects/index'),
+  //'project/performance-statistics': require('./components/project/performance-statistics'),
+};
