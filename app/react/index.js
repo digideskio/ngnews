a3980e730e2c78ec3d43dccfd4547b9c8fac4614
Sebastian Brych <sebastian.brych@netguru.pl>
2015-08-21 17:42:43 +0200

    Register item_list component

--- app/react/index.js
+++ app/react/index.js
@@ -1,5 +1,6 @@
 window.React = require('react');
 window.ReactComponents = {
+  'news/item_list': require('./components/news/item_list'),
   //Users: require('./components/users'),
   //'projects/index': require('./components/projects/index'),
   //'project/performance-statistics': require('./components/project/performance-statistics'),
