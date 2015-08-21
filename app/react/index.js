a56c9fdb150e9cbd49eb7bd6f57edda6930ff16b
Sebastian Brych <sebastian.brych@netguru.pl>
2015-08-21 18:14:14 +0200

    Add single item component

--- app/react/index.js
+++ app/react/index.js
@@ -1,5 +1,6 @@
 window.React = require('react');
 window.ReactComponents = {
+  'news/item': require('./components/news/item'),
   'news/item_list': require('./components/news/item_list'),
   //Users: require('./components/users'),
   //'projects/index': require('./components/projects/index'),
