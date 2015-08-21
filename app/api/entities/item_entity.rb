ab66a3e7e42c11f71cca73b80280bdacf039926d
Szymon Szeliga <szymon.szeliga@netguru.co>
2015-08-21 18:00:41 +0200

    Return formatted date

--- app/api/entities/item_entity.rb
+++ app/api/entities/item_entity.rb
@@ -1,6 +1,18 @@
 module Entities
   class ItemEntity < Grape::Entity
+    include ActionView::Helpers
     root 'items', 'item'
     expose :body, documentation: { type: "string", desc: "Content of the entry" }
+    expose :date do |item, options|
+      if item.is_event?
+        "#{item.start_date} - #{item.end_date}"
+      else
+        if item.start_date == Date.current
+          "Today..."
+        else
+          "#{time_ago_in_words(item.start_date)} ago"
+        end
+      end
+    end
   end
 end
